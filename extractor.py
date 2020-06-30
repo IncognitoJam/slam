4911d9475acbd8960f8491b3c200f195ca82daff
Cameron Clough <cameronjclough@gmail.com>
2020-06-30 16:49:52 +0100

    normalised coordinates

--- extractor.py
+++ extractor.py
@@ -1,15 +1,30 @@
 import cv2
 import numpy as np
-
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
 
+np.set_printoptions(suppress=True)
+
+
+def add_ones(x):
+    """Turn [[x, y]] into [[x, y, 1]"""
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+
 
 class Extractor:
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create()
         self.bf = cv2.BFMatcher_create(cv2.NORM_HAMMING)
         self.last = None
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+
+    def normalise(self, pts):
+        return np.dot(self.Kinv, add_ones(pts).T).T[:, 0:2]
+
+    def denormalise(self, pt):
+        ret = np.dot(self.K, np.array([pt[0], pt[1], 1.0]))
+        return int(round(ret[0])), int(round(ret[1]))
 
     def extract(self, img):
         # detection
@@ -33,12 +48,19 @@ class Extractor:
         # filter
         if len(ret) > 0:
             ret = np.array(ret)
+
+            # normalise coords: subtract to move to 0
+            ret[:, 0, :] = self.normalise(ret[:, 0, :])
+            ret[:, 1, :] = self.normalise(ret[:, 1, :])
+
             model, inliers = ransac((ret[:, 0], ret[:, 1]),
                                     FundamentalMatrixTransform,
                                     min_samples=8,
                                     residual_threshold=1,
                                     max_trials=100)
             ret = ret[inliers]
+            s, v, d = np.linalg.svd(model.params)
+            print(v)
 
         self.last = {'kps': kps, 'des': des}
         return ret
--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import cv2
-import time
-import sdl2
-import sdl2.ext
 import numpy as np
-import skimage
 
 from display import Display
 from extractor import Extractor
 
+W, H = 1920 // 2, 1080 // 2
+SIZE = (W, H)
+F = 1
+K = np.array(([F, 0, W // 2], [0, F, H // 2], [0, 0, 1]))
 
-size = (1920//2, 1080//2)
-display = Display(size)
-ex = Extractor()
+display = Display(SIZE)
+ex = Extractor(K)
 
 
 def process_frame(img):
-    img = cv2.resize(img, size)
+    img = cv2.resize(img, SIZE)
     matches = ex.extract(img)
 
     print("%d matches" % (len(matches)))
 
     for pt1, pt2 in matches:
-        u1, v1 = map(lambda x: int(round(x)), pt1)
-        u2, v2 = map(lambda x: int(round(x)), pt2)
+        u1, v1 = ex.denormalise(pt1)
+        u2, v2 = ex.denormalise(pt2)
         cv2.circle(img, (u1, v1), color=(0, 255, 0), radius=3)
         cv2.line(img, (u1, v1), (u2, v2), (255, 0, 0))
 
